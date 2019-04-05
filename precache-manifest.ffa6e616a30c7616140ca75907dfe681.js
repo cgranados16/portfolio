5829b5439cca026cf251683449f00913e975186f
Carlos Granados <cgranados16@gmail.com>
2019-04-04 22:32:13 -0600

    Updates

---
+++ precache-manifest.ffa6e616a30c7616140ca75907dfe681.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "6619b9792d7d108bb589b51bbd560278",
+    "url": "/portfolio/static/media/square2.6619b979.png"
+  },
+  {
+    "revision": "856d580404a33a399808",
+    "url": "/portfolio/static/css/main.c61328d3.chunk.css"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.784d01ab.js"
+  },
+  {
+    "revision": "8b956440016cf8463f5fc16a3073f432",
+    "url": "/portfolio/static/media/Map.8b956440.png"
+  },
+  {
+    "revision": "ee66a8542b4c6d569634",
+    "url": "/portfolio/static/js/2.92b59f61.chunk.js"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portfolio/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/portfolio/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/portfolio/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/portfolio/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/portfolio/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "b81dabc0bc6ca52fce921e3a928d5e0f",
+    "url": "/portfolio/static/media/dots.b81dabc0.png"
+  },
+  {
+    "revision": "ff004a0e5de54ba9dbd8583fa7e1b3f5",
+    "url": "/portfolio/static/media/square5.ff004a0e.png"
+  },
+  {
+    "revision": "856d580404a33a399808",
+    "url": "/portfolio/static/js/main.6ee35043.chunk.js"
+  },
+  {
+    "revision": "ca25707397fcc982d7dc1f82555f09a0",
+    "url": "/portfolio/static/media/square1.ca257073.png"
+  },
+  {
+    "revision": "23fa76237f0066f4ffd8f20e77449887",
+    "url": "/portfolio/static/media/square3.23fa7623.png"
+  },
+  {
+    "revision": "8fdf9ee20db40e5d1cf778bf99486cb7",
+    "url": "/portfolio/static/media/square4.8fdf9ee2.png"
+  },
+  {
+    "revision": "0f964a72f8fbdf9c8d4e0122b8effb40",
+    "url": "/portfolio/static/media/devicon.0f964a72.woff"
+  },
+  {
+    "revision": "29f80b00a7b4641183f96f08be374697",
+    "url": "/portfolio/static/media/devicon.29f80b00.ttf"
+  },
+  {
+    "revision": "681fa39a79c22f0035a0720e2b2bda3b",
+    "url": "/portfolio/static/media/devicon.681fa39a.eot"
+  },
+  {
+    "revision": "74e6e6c3dcfca56767adabe83a510f2e",
+    "url": "/portfolio/static/media/devicon.74e6e6c3.svg"
+  },
+  {
+    "revision": "7964fa9025c306144ff6dbc99898ef81",
+    "url": "/portfolio/static/media/start.7964fa90.png"
+  },
+  {
+    "revision": "b8433347533b1c75066ff210096c49ec",
+    "url": "/portfolio/static/media/game.b8433347.png"
+  },
+  {
+    "revision": "ce31c64f7b1e1dd78036c1c319f8394a",
+    "url": "/portfolio/static/media/Landing.ce31c64f.PNG"
+  },
+  {
+    "revision": "2815f33b30a5df5ff61f9e759648dd3b",
+    "url": "/portfolio/static/media/harvest-page.2815f33b.PNG"
+  },
+  {
+    "revision": "967370570856732a538041422b8f97e9",
+    "url": "/portfolio/static/media/reviews.96737057.PNG"
+  },
+  {
+    "revision": "ed4ee7b3921bbfa49481d1ff95959049",
+    "url": "/portfolio/static/media/sales.ed4ee7b3.png"
+  },
+  {
+    "revision": "ee66a8542b4c6d569634",
+    "url": "/portfolio/static/css/2.e88d9f69.chunk.css"
+  },
+  {
+    "revision": "f19d2c80c8798f7b22874211a084ce0c",
+    "url": "/portfolio/index.html"
+  }
+];
